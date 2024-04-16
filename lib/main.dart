159e68eadbb67476b15a63516bab3eae94e9e5cd
Peter Kim <offpeterhk@gmail.com>
2024-04-16 15:09:42 -0400

    UI Changes
    Added dark theme, minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,27 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { //navigation builder, chooses what screen the app will initialize on
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Power Monitor',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.grey[900],        // blue as the accent color
+        scaffoldBackgroundColor: Colors.grey[850],
+        appBarTheme: AppBarTheme(
+          color: Colors.grey[900],
+          elevation: 0,
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue,  // Buttons will have a blue color
+          textTheme: ButtonTextTheme.primary,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.green, backgroundColor: Colors.grey[800],   // Button text color for light themes
+          ),
+        ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.blue),
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => MainScreen(),
